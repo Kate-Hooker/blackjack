ba03857de6f63dd0e5851abd36b7981dff1282cc
Kate Hooker <katehooker88@gmail.com>
2023-07-18 18:15:59 +1200

    dealers hand works?

--- blackjack.js
+++ blackjack.js
@@ -1,30 +1,26 @@
-/* build deck, shuffle deck, select one card at a time
-make dealers hand - total their cards - if less than 17 take a third.
-keep one face down
-charge ace as 11, count aces, deduct if over 21 
-make player total
-buttons for stay and hit
-automatic loss if over 21
-if both under 21 closest wins
-if deler over 21 and player under player wins
--- functions to make - build deck (then shuffle deck)
-deal out hands - auto logic for computer to make its move - computer hand check for hit, count aces,
-*/
 const randomNumber = Math.floor(Math.random() * 53)
 let shuffledDeck = []
 let freshCard = []
 let dealerSecretCard = []
-let dealersHand = []
-let playerHand = []
+let dealerHand = []
+let playersHand = []
 
 let dealerScore = 0
 let dealerCardsTotal = 0
 let playerScore = 0
 let playerCardsTotal = 0
 
-var valueOnCard
-var suiteIdentifier
-var cardPoints
+let cardPoints
+let nominateParticipant
+let participant
+let card
+let cardParts
+let valueOnCard
+let suiteIdentifier
+
+let dealerAces = 0
+let playerAces = 0
+let numberOfAces = 0
 
 //build a shuffled deck with all 52 cards
 function buildDeck() {
@@ -46,6 +42,7 @@ function buildDeck() {
   let cardSuits = ['D', 'H', 'S', 'C']
 
   let deck = []
+  shuffledDeck = []
 
   for (let i = 0; i < cardValues.length; i++) {
     for (let j = 0; j < cardSuits.length; j++) {
@@ -61,55 +58,120 @@ function dealerGetsAFaceDownCard() {
   dealerSecretCard = document.querySelector('#faceDown')
   dealACard()
   dealerSecretCard = freshCard
-  dealersHand.push(dealerSecretCard)
+  dealerHand.push(dealerSecretCard)
   console.log(dealerSecretCard + ' is the dealers card')
 }
 //pulls a card and adds it to player ones hand, return shuffled deck without card
 //retrieve card, break into string and number, use that to pick image
 function dealACard() {
   freshCard = shuffledDeck.pop()
+  card = freshCard
 }
 
-function calculateCardScore() {
-  const cardParts = freshCard.split('-')
+// ie calculateScore('dealer', dealerHand)
+function calculateScore(nominateParticipant, hand) {
+  let score = 0
 
+  for (let i = 0; i < hand.length; i++) {
+    const card = hand[i]
+    const cardParts = card.split('-')
+    const valueOnCard = cardParts[0]
+    const suiteIdentifier = cardParts[1]
+
+    if (valueOnCard === 'K' || valueOnCard === 'Q' || valueOnCard === 'J') {
+      cardPoints = 10
+    } else if (valueOnCard === 'A') {
+      cardPoints = 11
+      if (nominateParticipant === 'dealer') {
+        dealerAces++
+      } else if (nominateParticipant === 'player') {
+        playerAces++
+      }
+    } else {
+      cardPoints = parseInt(valueOnCard)
+    }
+
+    score += cardPoints
+  }
+
+  if (nominateParticipant === 'dealer') {
+    dealerScore += score
+  } else if (nominateParticipant === 'player') {
+    playerScore += score
+  }
+
+  console.log('Total dealer points: ' + dealerScore)
+}
+
+function takeAnotherCard() {
+  dealACard()
+
+  dealerHand.push(freshCard)
+
+  let dealerCards = document.querySelector('.dealerCards')
+  cardParts = card.split('-')
   valueOnCard = cardParts[0]
   suiteIdentifier = cardParts[1]
+  // Create an image element
+  const cardImage = document.createElement('img')
+  cardImage.src = `cards/${valueOnCard}-${suiteIdentifier}.png`
 
-  if (cardParts[0] === 'K' || cardParts[0] === 'Q' || cardParts[0] === 'J') {
-    cardPoints = 10
-  }
-  if (cardParts[0] === 'A') {
-    cardPoints = 11
-  } else {
-    cardPoints = parseInt(cardParts[0])
+  //Append the image to the dealerCards container
+  dealerCards.appendChild(cardImage)
+
+  calculateScore('dealer', dealerHand)
+  processDealerAces()
+  console.log('this is very unlikely ' + dealerHand)
+}
+
+function processDealerAces() {
+  if (dealerScore < 17) {
+    takeAnotherCard()
   }
 
-  console.log('Card: ' + valueOnCard)
-  console.log('Points: ' + cardPoints)
+  while (numberOfAces > 0 && dealerScore > 21) {
+    dealerScore -= 10
+    numberOfAces--
+    if (dealerScore >= 17 && dealerScore <= 21) {
+      return dealerScore
+    }
+    if (dealerScore < 17) {
+      takeAnotherCard()
+      return dealerScore
+    }
+  }
 }
 
 //start when the table opens. One card face up displayed, on facedown displayed, if less than
 //
 
 function handleStartOverButtonClick() {
+  shuffledDeck = []
+  freshCard = []
+  dealerSecretCard = []
+  playersHand = []
+
   buildDeck()
   dealACard()
-  calculateCardScore()
 
-  dealersHand.push(freshCard)
+  dealerHand.push(freshCard)
 
   let dealerCards = document.querySelector('.dealerCards')
-
+  cardParts = card.split('-')
+  valueOnCard = cardParts[0]
+  suiteIdentifier = cardParts[1]
   // Create an image element
   const cardImage = document.createElement('img')
   cardImage.src = `cards/${valueOnCard}-${suiteIdentifier}.png`
 
-  // Append the image to the dealerCards container
+  //Append the image to the dealerCards container
   dealerCards.appendChild(cardImage)
 
   dealerGetsAFaceDownCard()
-  console.log('this should be the dealers array ' + dealersHand)
+  console.log('this should be the dealers array ' + dealerHand)
+
+  calculateScore('dealer', dealerHand)
+  processDealerAces()
 }
 
 const startOverButton = document.getElementById('startOver')
