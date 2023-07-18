c5fecdc1f185ea186fbf7668475dd8ec901eb739
Kate Hooker <katehooker88@gmail.com>
2023-07-19 00:01:42 +1200

    blackjack generates dealers turn, keeps score, some logic

--- blackjack.js
+++ blackjack.js
@@ -6,13 +6,13 @@ let dealerHand = []
 let playersHand = []
 
 let dealerScore = 0
+let dealerScoreTwo = { score: 0, aces: 0 }
 let dealerCardsTotal = 0
 let playerScore = 0
 let playerCardsTotal = 0
 
 let cardPoints
 let nominateParticipant
-let participant
 let card
 let cardParts
 let valueOnCard
@@ -72,6 +72,7 @@ function dealACard() {
 // ie calculateScore('dealer', dealerHand)
 function calculateScore(nominateParticipant, hand) {
   let score = 0
+  let aces = 0
 
   for (let i = 0; i < hand.length; i++) {
     const card = hand[i]
@@ -79,28 +80,32 @@ function calculateScore(nominateParticipant, hand) {
     const valueOnCard = cardParts[0]
     const suiteIdentifier = cardParts[1]
 
-    let cardPoints
+    let cardPoints = 0
 
     if (valueOnCard === 'K' || valueOnCard === 'Q' || valueOnCard === 'J') {
       cardPoints = 10
     } else if (valueOnCard === 'A') {
       cardPoints = 11
-      if (nominateParticipant === 'dealer') {
-        dealerAces++
-      } else if (nominateParticipant === 'player') {
-        playerAces++
-      }
+      aces++
+      // if (nominateParticipant === 'dealer') {
+      // dealerAces++
+      // } else if (nominateParticipant === 'player') {
+      // playerAces++
+      //}
     } else {
       cardPoints = parseInt(valueOnCard)
     }
-
+    //seperate dealer/player here
     score += cardPoints
   }
-
-  return score
+  //if (nominateParticipant === 'dealer') {
+  // dealerScore += score
+  //else
+  return { score: score, aces: aces }
+  //return score
 }
 
-function takeAnotherCard() {
+function dealersTurn() {
   dealACard()
 
   dealerHand.push(freshCard)
@@ -116,14 +121,56 @@ function takeAnotherCard() {
   //Append the image to the dealerCards container
   dealerCards.appendChild(cardImage)
 
-  calculateScore('dealer', dealerHand)
-  processDealerAces()
-  console.log('it would be great if this is the right score ' + dealerScore)
+  dealerScoreTwo = calculateScore('dealer', dealerHand)
+  //processAces(dealerScore)
+  dealerScoreTwo = processAces(dealerScoreTwo)
+
+  console.log(
+    'this is the score at the end of dealers turn' + dealerScoreTwo.score
+  )
+  console.log(
+    'this is dealerscortwoe at the end of dealers turn' + dealerScoreTwo
+  )
+  while (dealerScoreTwo.score < 17) {
+    dealersTurn()
+  }
+
+  //dealerScore = calculateScore('dealer', dealerHand)
+  //processAces(dealerScore)
+  console.log(
+    'it would be great if this is the right score ' + dealerScoreTwo.score
+  )
   console.log('this is very unlikely ' + dealerHand)
 }
 
+function processAces(score) {
+  console.log('process aces new fuction points: ' + dealerScore)
+
+  //dealerScore = score
+
+  //if (dealerScore < 17) {
+  //  takeAnotherCard()
+  //}
+  let numberOfAces = score.aces
+  while (numberOfAces > 0 && score.score >= 21) {
+    score.score -= 10
+    numberOfAces--
+    /*if (dealerScore >= 17 && dealerScore <= 21) {
+      return dealerScore
+    }
+    if (dealerScore < 17) {
+      takeAnotherCard() 
+      return dealerScore
+    } */
+  }
+  return score
+}
+
 function processDealerAces() {
-  console.log('Totprocess aces dealer points: ' + dealerScore)
+  console.log('process aces dealer points: ' + dealerScore)
+
+  dealerScore = score
+
   if (dealerScore < 17) {
     takeAnotherCard()
   }
@@ -185,8 +232,7 @@ function handleStartOverButtonClick() {
   dealerGetsAFaceDownCard()
   console.log('this should be the dealers array ' + dealerHand)
 
-  calculateScore('dealer', dealerHand)
-  processDealerAces()
+  dealersTurn()
 }
 
 const startOverButton = document.getElementById('startOver')
